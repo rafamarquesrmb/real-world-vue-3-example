77976a3be26e098e8f0d5a69ce9e5ca126392ffb
rafamarquesrmb <rafa.rmb@gmail.com>
2022-07-22 17:36:50 -0300

    Initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style>
@@ -26,5 +28,8 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+h4 {
+  font-size: 20px;
 }
 </style>
