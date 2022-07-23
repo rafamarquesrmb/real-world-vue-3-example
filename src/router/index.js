d22c36ccfafcd5e1fe8dd364029dd1b460c69df9
rafamarquesrmb <rafa.rmb@gmail.com>
2022-07-23 02:17:11 -0300

    Using Nested Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import AboutView from "../views/AboutView.vue";
-import EventDetails from "../views/EventDetails.vue";
-
+import EventDetails from "../views/event/EventDetails.vue";
+import EventEdit from "../views/event/EventEdit.vue";
+import EventRegister from "../views/event/EventRegister.vue";
+import EventLayout from "../views/event/EventLayout.vue";
 const routes = [
   {
     path: "/",
@@ -19,9 +21,26 @@ const routes = [
   },
   {
     path: "/event/:id",
-    name: "EventDetails",
+    name: "EventLayout",
     props: true,
-    component: EventDetails,
+    component: EventLayout,
+    children: [
+      {
+        path: "",
+        name: "EventDetails",
+        component: EventDetails,
+      },
+      {
+        path: "register",
+        name: "EventRegister",
+        component: EventRegister,
+      },
+      {
+        path: "edit",
+        name: "EventEdit",
+        component: EventEdit,
+      },
+    ],
   },
 ];
 
--- src/views/event/EventLayout.vue
+++ src/views/event/EventLayout.vue
@@ -1,8 +1,22 @@
 <template>
   <div v-if="event">
     <h1>{{ event.title }}</h1>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-    <p>{{ event.description }}</p>
+
+    <div id="nav">
+      <router-link :to="{ name: 'EventDetails', params: { id } }"
+        >Details</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventRegister', params: { id } }"
+        >Register</router-link
+      >
+      |
+      <router-link :to="{ name: 'EventEdit', params: { id } }"
+        >Edit</router-link
+      >
+    </div>
+
+    <router-view :event="event" />
 
     <router-link :to="{ name: 'EventList' }">Return to events list</router-link>
   </div>
