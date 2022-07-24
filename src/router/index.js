bb51659d2658072642d29240a2b68455a935c031
rafamarquesrmb <rafa.rmb@gmail.com>
2022-07-24 00:04:42 -0300

    Not Found page

--- src/router/index.js
+++ src/router/index.js
@@ -5,52 +5,63 @@ import EventDetails from "../views/event/EventDetails.vue";
 import EventEdit from "../views/event/EventEdit.vue";
 import EventRegister from "../views/event/EventRegister.vue";
 import EventLayout from "../views/event/EventLayout.vue";
+import NotFound from "../views/NotFound.vue";
 const routes = [
-  {
-    path: "/",
-    name: "EventList",
-    component: EventList,
-    props: (route) => ({
-      page: parseInt(route.query.page) || 1,
-    }),
-  },
-  {
-    path: "/about-us",
-    name: "About",
-    component: AboutView,
-  },
-  {
-    path: "/about-us",
-    redirect: { name: "About" },
-  },
-  {
-    path: "/events/:id",
-    name: "EventLayout",
-    props: true,
-    component: EventLayout,
-    children: [
-      {
-        path: "",
-        name: "EventDetails",
-        component: EventDetails,
-      },
-      {
-        path: "register",
-        name: "EventRegister",
-        component: EventRegister,
-      },
-      {
-        path: "edit",
-        name: "EventEdit",
-        component: EventEdit,
-      },
-    ],
-  },
+    {
+        path: "/",
+        name: "EventList",
+        component: EventList,
+        props: (route) => ({
+            page: parseInt(route.query.page) || 1,
+        }),
+    },
+    {
+        path: "/:catchall(.*)",
+        name: "NotFound",
+        component: NotFound,
+    },
+    {
+        path: "/about-us",
+        name: "About",
+        component: AboutView,
+    },
+    {
+        path: "/about-us",
+        redirect: { name: "About" },
+    },
+    {
+        path: "/events/:id",
+        name: "EventLayout",
+        props: true,
+        component: EventLayout,
+        children: [
+            {
+                path: "",
+                name: "EventDetails",
+                component: EventDetails,
+            },
+            {
+                path: "register",
+                name: "EventRegister",
+                component: EventRegister,
+            },
+            {
+                path: "edit",
+                name: "EventEdit",
+                component: EventEdit,
+            },
+        ],
+    },
+    {
+        path: "/:catchall(.*)",
+        name: "NotFound",
+        component: NotFound,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
